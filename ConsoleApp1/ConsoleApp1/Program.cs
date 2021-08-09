2f4d82733ccba8390d587480ee802b96f7758def
Luisa Gerónimo <luisageronimo5@gmail.com>
2021-08-09 16:06:41 -0600

    Aplicacion 1 terminado

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aplicacion1_4taunidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string archivo;
+            int p;
+            string R = "s";
+            while (R == "s")
+            { 
+                Console.WriteLine("Que desea hacer\n1. Crear \n2. Salir");
+                 p = int.Parse(Console.ReadLine());
+
+
+                if (p==1)
+                { 
+                 StreamWriter ar;
+                 archivo = Console.ReadLine();
+                 Console.WriteLine("Escribe el nombre del arhivo: ");
+                 ar = File.CreateText(Console.ReadLine() + ".txt");
+                 ar.Close();
+                }
+               Console.Clear();
+
+              if (p == 2)
+              {
+                Console.WriteLine("Usted a deseado salir");
+              }
+              Console.Clear();
+              Console.WriteLine("Quieres crear otro archivo?");
+              Console.WriteLine("s/n");
+              R = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
